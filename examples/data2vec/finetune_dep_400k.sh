0f9b03ff2d8b3a4590d693032f55fe694902e3a5
Alexander-H-Liu <alexliu36@gmail.com>
2023-01-05 21:13:41 -0500

    2nd round sweep

---
+++ examples/data2vec/finetune_dep_400k.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env zsh
+
+job_id="$1"
+task_id="$2"
+dir="$3"
+
+dep_str=""
+if [ ! -z "${job_id}" ]; then
+  dep_str="+hydra.launcher.additional_parameters.dependency=afterok:$job_id"
+fi
+echo "job_id: $job_id, task_id: $task_id, dir: $dir"
+echo "dep_str: ${dep_str}"
+
+n_gpu_total=8
+n_cpu=6
+update_freq='[1]'
+port=12347
+
+# Env (python env identical to data2vec)
+fairseq_root="/private/home/wnhsu/data2vec_quant/fairseq-dev"
+log_root="/checkpoint/wnhsu/projects/data2vec_quant/exps" # same as train_200k.sh
+data_root="/checkpoint/wnhsu/data/librispeech/10h/raw" # Path to data tsv files (e.g., train 10hr)
+lm_path="/checkpoint/wnhsu/data/librispeech/lm_4gram/4-gram.bin" # I'm usomg official 4-gram.bin 
+lexicon_path="/checkpoint/wnhsu/data/librispeech/lm_4gram/lexicon_ltr.lst" # I'm using official librispeech_lexicon.lst
+
+# Finetuning options
+split="train" # finetuning {split}.tsv
+valid_subset="dev_other" # tsv file name for validation
+config_path=$fairseq_root/examples/wav2vec/config/finetuning/ # might wanna try hubert config in the furture
+config=base_10h # depending on {split}
+load_teacher_model=true # true: finetune teacher model; false: student
+
+finetune_ckpts=( "checkpoint_214_200000.pt" "checkpoint_last.pt" ) # Eval @ 200k & 400k
+
+
+for $ft_ckpt in ${finetune_ckpts[@]};do
+    # Paths and pre-trained checkpoints
+    pretrain_ckpt_path="${dir}/ckpt/${ft_ckpt}"
+    log_dir="${dir}/finetune/10h/${ft_ckpt}_teacher${load_teacher_model}"
+
+    mkdir -p $log_dir
+
+    set -x
+    PYTHONPATH=$(pwd):$(pwd)/examples LOG_DIR=${log_dir} \
+    python fairseq_cli/hydra_train.py -m \
+    --config-dir $config_path \
+    --config-name $config \
+    hydra/launcher=submitit_slurm +run=slurm_1 $dep_str \
+    common.log_file=$log_dir/log.txt \
+    common.tensorboard_logdir=$log_dir/tb \
+    common.user_dir=$fairseq_root/examples/data2vec \
+    checkpoint.save_dir=$log_dir/ckpt \
+    checkpoint.save_interval=5 dataset.validate_interval=5 \
+    dataset.train_subset=$split \
+    dataset.valid_subset=$valid_subset \
+    dataset.num_workers=$n_cpu \
+    model.w2v_path=$pretrain_ckpt_path \
+    +model.load_teacher_model=$load_teacher_model \
+    distributed_training.distributed_world_size=$n_gpu_total \
+    distributed_training.distributed_port=$port \
+    +optimization.update_freq=$update_freq \
+    task.data=$data_root \
+    task.normalize=true \
+    +criterion.wer_lexicon=$lexicon_path \
+    +criterion.wer_kenlm_model=$lm_path \
+    +criterion.wer_lm_weight=2.0 \
+    +criterion.wer_word_score=-1.0 &
+done
---
+++ examples/data2vec/train_400k_cascade_tune.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env zsh
+
+# Setting used in MIT (default 32GB GPU x16)
+n_gpu_total=16
+n_cpu=6
+update_freq='[1]' # Avoid using update_freq>1 since codebook is updated per forward pass
+port=12347
+
+# Env (python env identical to data2vec)
+fairseq_root="/private/home/wnhsu/data2vec_quant/fairseq-dev"
+data_root="/checkpoint/wnhsu/data/hubert/manifests/pretrain/data/ls960/" # Path to data tsv files (libri 960hr)
+train_subset="train"
+valid_subset="dev_other" # tsv file name for validation
+
+# Log
+log_root="/checkpoint/wnhsu/projects/data2vec_quant/exps"
+log_interval=200
+save_every_n_updates=50000 # hopefully % 200k == 0
+keep_interval_updates=-1 # keep ckpts if space permit 
+
+# Hyper-param. (prioritized in order)
+### Size of codebook
+codebook_size=$1  # 512>1024>2048>256>128
+### Targeting top K layers, each with a codebook
+top_k=$2 # 8>10>6>12>4>2 (not sure if 10 & 12 is going to run OOM)
+### Codebook init. method
+normal_init_codebook="$3" # false: random then Instance norm, true: normal(0,1/|codebook|**2)
+### Masking prob
+mask_prob="$4"      # worth tuning?
+### TBD
+# freeze_codebook_step=200000 # see if training is unstable before 200k
+codebook_init_decay="$5" # so far so good (unstable is caused by LR or teacher EMA, not codebook decay)
+
+log_dir=$log_root/pretrain_400k/base_discrete_v${codebook_size}_norm${normal_init_codebook}_k${top_k}_m${mask_prob}_cbdec${codebook_init_decay}
+mkdir -p $log_dir
+
+set -x
+PYTHONPATH=$(pwd):$(pwd)/examples LOG_DIR=${log_dir} \
+python fairseq_cli/hydra_train.py -m \
+--config-dir $fairseq_root/examples/data2vec/config/audio/pretraining \
+--config-name base_discrete_400k \
+hydra/launcher=depedency_submitit_slurm +run=slurm_2 \
++next_script=${fairseq_root}/examples/data2vec/finetune_dep_400k.sh \
+task.data=$data_root \
+dataset.train_subset=$train_subset \
+dataset.valid_subset=$valid_subset \
+dataset.num_workers=$n_cpu \
+common.log_interval=$log_interval \
+common.log_file=$log_dir/log.txt \
+common.tensorboard_logdir=$log_dir/tb \
+common.user_dir=$fairseq_root/examples/data2vec \
+checkpoint.save_dir=$log_dir/ckpt \
+checkpoint.save_interval_updates=$save_every_n_updates \
+checkpoint.keep_interval_updates=$keep_interval_updates \
+distributed_training.distributed_world_size=$n_gpu_total \
+distributed_training.distributed_port=$port \
+model.codebook_size=$codebook_size \
+model.normal_init_codebook=$normal_init_codebook \
+model.average_top_k_layers=$top_k \
+model.mask_prob=$mask_prob \
+model.codebook_init_decay=$codebook_init_decay \
++optimization.update_freq=$update_freq &
